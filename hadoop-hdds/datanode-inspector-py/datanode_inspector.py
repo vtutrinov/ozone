420b230845f2960b08b11352a33a0fa0324ab4cf
Slava Tutrinov <vyacheslav.tutrinov@wandisco.com>
2024-01-23 18:20:32 +0300

    per container report

--- hadoop-hdds/datanode-inspector-py/datanode_inspector.py
+++ hadoop-hdds/datanode-inspector-py/datanode_inspector.py
@@ -24,30 +24,50 @@ def get_container_report():
             for block_file in glob.iglob(containers[container_id]["containerPath"] + "/**/*.block"):
                 containers[container_id]["orphanedBlocks"].append(block_file)
 
-    for dbFile in db_files:
-        chunks_scan_command_output = subprocess.check_output(["ozone", "debug", "ldb", "--db=" + dbFile, "scan", "--cf=" + "block_data"])
-        blocks_data = json.loads(chunks_scan_command_output)
+
+    db_files = list(db_files)
+    block_report_files = []
+    for container_id in containers:
+        container_db_file = containers[container_id]["dbFile"]
+        blocks_report_file = container_db_file.replace("/", "_") + ".json"
+        if blocks_report_file not in block_report_files:
+            db_file_index = db_files.index(container_db_file)
+            chunks_scan_command_output = json.loads(subprocess.check_output(["ozone", "debug", "ldb", "--db=" + container_db_file, "scan", "--cf=" + "block_data"]))
+            del db_files[db_file_index]
+            block_report_files.append(blocks_report_file)
+        else:
+            with open(blocks_report_file, "r") as fp:
+                chunks_scan_command_output = json.load(fp, "encodings.utf_8")
+
+        blocks_data = chunks_scan_command_output
+        handled_blocks = []
         for block_id in blocks_data:
-            block_id_parts = block_id.split("|")
-            container_id = block_id_parts[0]
-            block_local_id = block_id_parts[1]
+            block_data = blocks_data[block_id]
+            container_id = str(block_data['blockID']['containerBlockID']['containerID'])
+            block_local_id = str(block_data['blockID']['containerBlockID']['localID'])
             containers[container_id]["blocks"][block_local_id] = blocks_data[block_id]
             containers[container_id]["blocks"][block_local_id]["blockFile"] = containers[container_id]["chunksPath"] + "/" + block_local_id + ".block"
             block_file_exists = os.path.isfile(containers[container_id]["blocks"][block_local_id]["blockFile"])
             containers[container_id]["blocks"][block_local_id]["blockFileExists"] = block_file_exists
             if block_file_exists:
                 containers[container_id]["orphanedBlocks"].remove(containers[container_id]["blocks"][block_local_id]["blockFile"])
+            handled_blocks.append(block_id)
+        for bid in handled_blocks:
+            del blocks_data[bid]
 
-    return containers
+        with open(blocks_report_file, "w") as brf:
+            json.dump(blocks_data, brf, indent=4)
+        block_report_files.append(blocks_report_file)
 
+        with open("container_" + container_id + ".json", "w") as cf:
+            container_object = {container_id: containers.pop(container_id)}
+
+            json.dump(container_object, cf, indent=4)
+            containers[container_id] = {}
+    print containers
 
 args_parser = argparse.ArgumentParser(description="Generate datanode containers report")
 args_parser.add_argument("--stdout", dest="stdout", action='store_true')
 args_parser.add_argument("--report-file", dest="report_file", default="report.json")
 args = args_parser.parse_args()
 report = get_container_report()
-if args.stdout:
-    print(json.dumps(report, indent=4))
-else:
-    with open(args.report_file, 'w') as f:
-        json.dump(report, f, indent=4)
