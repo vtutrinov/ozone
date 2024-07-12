7a63b87aa0b5c544c079b12bfb71922b11791bec
Kirill Sizov <KYuSizov2@sberbank.ru>
2024-07-12 12:26:17 +0300

    HDDS-10250. Use SnapshotId as key in SnapshotCache

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/snapshot/OMSnapshotPurgeRequest.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/request/snapshot/OMSnapshotPurgeRequest.java
@@ -136,7 +136,7 @@ public class OMSnapshotPurgeRequest extends OMClientRequest {
               updatedPathPreviousAndGlobalSnapshots);
           updateSnapshotInfoAndCache(nextSnapshot, omMetadataManager, trxnLogIndex, updatedSnapInfos);
           // Remove and close snapshot's RocksDB instance from SnapshotCache.
-          ozoneManager.getOmSnapshotManager().invalidateCacheEntry(snapTableKey);
+          ozoneManager.getOmSnapshotManager().invalidateCacheEntry(fromSnapshot.getSnapshotId());
           // Update SnapshotInfoTable cache.
           omMetadataManager.getSnapshotInfoTable()
               .addCacheEntry(new CacheKey<>(fromSnapshot.getTableKey()), CacheValue.get(trxnLogIndex));
