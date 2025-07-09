032004d3724fa9959db7bc4588a0de47ad151d49
Юнцевич Александр Сергеевич <22267276@sigma.sbrf.ru>
2025-07-09 13:19:24 +0300

    [SDPOZN-1708] Fix review notices

--- hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/helpers/OmBucketInfo.java
+++ hadoop-ozone/common/src/main/java/org/apache/hadoop/ozone/om/helpers/OmBucketInfo.java
@@ -694,9 +694,6 @@ public final class OmBucketInfo extends WithObjectID implements Auditable, Bucke
     if (raftGroup != null) {
       bib.setRaftGroup(HddsUtils.toProtobuf(raftGroup));
     }
-    if (raftGroup != null) {
-      bib.setRaftGroup(HddsUtils.toProtobuf(raftGroup));
-    }
     return bib.build();
   }
 
