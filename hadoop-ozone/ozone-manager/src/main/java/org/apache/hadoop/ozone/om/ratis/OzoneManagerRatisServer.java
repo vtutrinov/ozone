500a950b17a30b04787501d17eab894b26709924
Slava Tutrinov <vyacheslav.tutrinov@wandisco.com>
2026-01-19 12:57:55 +0300

    [MULTIRAFT] sync work between raft groups leadership balancer and the task of the groups reconsillation

--- hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/ratis/OzoneManagerRatisServer.java
+++ hadoop-ozone/ozone-manager/src/main/java/org/apache/hadoop/ozone/om/ratis/OzoneManagerRatisServer.java
@@ -1092,7 +1092,7 @@ public final class OzoneManagerRatisServer {
   public void startSchedulingLeaderReconfiguration() {
     scheduler = Executors.newSingleThreadScheduledExecutor();
     scheduler.scheduleAtFixedRate(
-            new LeaderCheckExecutor(this, ozoneManager.getConfiguration()),
+            new LeaderCheckExecutor(this, ozoneManager.getConfiguration(), ozoneManager.getOmRaftGroupManager()),
             groupTransferLeadershipSchedulingInitialDelay, groupTransferLeadershipSchedulingPeriod, TimeUnit.SECONDS);
   }
 
